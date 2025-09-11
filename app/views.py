f8b928e06b5df8f70ce4603effe5f77fc1c9d57d
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2025-09-11 20:01:19 -0300

    back-end
    criação das tabelas de check-in massagem e limpeza
    
    falta conectá-las a tabela cliente

--- app/views.py
+++ app/views.py
@@ -18,11 +18,20 @@ def init_db():
         nome TEXT NOT NULL,
         status_massagem BOOLEAN DEFAULT FALSE,
         status_limpeza BOOLEAN DEFAULT FALSE,
-        checkins INTEGER DEFAULT 0
+        checkins_massagem INTEGER DEFAULT 0,
+        checkins_limpeza INTEGER DEFAULT 0
     )
     ''')
     conn.execute('''
-    CREATE TABLE IF NOT EXISTS checkins (
+    CREATE TABLE IF NOT EXISTS checkins_massagem (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cliente_id INTEGER,
+        data TEXT,
+        FOREIGN KEY(cliente_id) REFERENCES clientes(id)
+    )
+    ''')
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS checkins_limpeza (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         cliente_id INTEGER,
         data TEXT,
@@ -76,6 +85,28 @@ def registrar_checkin(cliente_id):
         conn.execute("UPDATE clientes SET status_limpeza = ? WHERE id = ?", (status_limpeza,cliente_id))
         conn.execute("INSERT INTO checkins (cliente_id, data) VALUES (?, ?)",
                      (cliente_id, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        conn.commit()
+    conn.close()
+    return redirect(url_for('homepage'))
+
+@app.route('/checkinmassagem/<int:cliente_id>')
+def registrar_checkin_massagem(cliente_id):
+    conn = get_db_connection()
+    cliente = conn.execute("SELECT * FROM clientes WHERE id = ?", (cliente_id,)).fetchone()
+    status_massagem = False
+
+    if cliente:
+        novos_checkins = cliente['checkins'] + 1
+        if novos_checkins >= 3:
+            status_massagem = True
+        else:
+            novos_checkins = 0
+            status_massagem = False
+    
+        conn.execute("UPDATE clientes SET checkins = ? WHERE id = ?", (novos_checkins, cliente_id))
+        conn.execute("UPDATE clientes SET status_massagem = ? WHERE id = ?", (status_massagem,cliente_id))
+        conn.execute("INSERT INTO checkins_massagem (cliente_id, data) VALUES (?, ?)",
+                        (cliente_id, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
         conn.commit()
     conn.close()
     return redirect(url_for('homepage'))
